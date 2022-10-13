eac534844f5e56a73e33558a8570193b9effa6e1
hara sogo <hara@honeycomb-lab.co.jp>
2022-10-13 16:37:13 +0900

    add: アドバタイズ受信した端末の名前がログで確認できるようになった

--- AdvertiseTestApp/ViewController.swift
+++ AdvertiseTestApp/ViewController.swift
@@ -1,19 +1,66 @@
-//
-//  ViewController.swift
-//  AdvertiseTestApp
-//
-//  Created by hara on 2022/10/07.
-//
-
 import UIKit
+import CoreBluetooth
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var scanBtn: UIButton!
+    @IBOutlet weak var stopBtn: UIButton!
+    private var centralManager: CBCentralManager!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        scanBtn.addTarget(self, action: #selector(self.scanButton(_:)), for: UIControl.Event.touchUpInside)
+        stopBtn.addTarget(self, action: #selector(self.stopButton(_:)), for: UIControl.Event.touchUpInside)
+        centralManager = CBCentralManager(delegate: nil, queue: nil)
+        centralManager.delegate = self
+    }
+    
+
+    @IBAction func scanButton(_ sender: Any) {
+        scan()
     }
 
+    @IBAction func stopButton(_ sender: Any) {
+        stopScan()
+    }
 
+    func deviceLabel(string: String) {
+        print(string)
+    }
+    
+    func scan() {
+        centralManager.scanForPeripherals(withServices: nil, options: nil)
+    }
+
+    func stopScan() {
+        centralManager.stopScan()
+    }
+
+    func connect(peripheral: CBPeripheral) {
+        centralManager.connect(peripheral, options: nil)
+    }
+
+    func disconnect(peripheral: CBPeripheral) {
+        centralManager.cancelPeripheralConnection(peripheral)
+    }
 }
 
+extension ViewController: CBCentralManagerDelegate {
+
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+//        print("state: \(central.state.rawValue)")
+    }
+
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+        deviceLabel(string: "pheripheral.name: \(String(describing: peripheral.name))" + "\n" + "advertisementData:\(advertisementData.count)" + "\n" +
+                "RSSI: \(RSSI)" + "\n")
+    }
+
+    func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
+        
+    }
+
+    func centralManager(_ central: CBCentralManager, didDisconnectPeripheral peripheral: CBPeripheral, error: Error?) {
+        
+    }
+}
